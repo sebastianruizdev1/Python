e8591b6e4b9c12cee30abdd6edf9a87ff87b4f61
sebastianruizdev1 <sebastianruizdev@gmail.com>
2025-03-19 19:13:18 -0600

    second commit

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,4 @@
+#Aqui se imprime un mensaje de "Hello World"
+#Autor: Sebastián Ruiz - @sebastianruizdev1
+#Fecha: 19/03/2025
 print("Hello World")
---
+++ operacionesBasicas.py
@@ -0,0 +1,11 @@
+#Operaciones Básicas: Suma y Multiplicación
+#Descripción: Realiza operaciones aritméticas básicas como suma y multiplicación.
+#Objetivo: Entender cómo realizar operaciones matemáticas y usar variables.
+#Autor: Sebastián Ruiz - @sebastianruizdev1
+#Fecha: 19/03/2025
+num1 = 5
+num2 = 3
+suma = num1 + num2
+multiplicacion = num1 * num2
+print("La suma de", num1, "y", num2, "es igual a", suma)
+print("La multiplicación de", num1, "y", num2, "es igual a", multiplicacion)
